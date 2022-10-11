2554f7dfd41e1c5a9443c97aed06e42e7707f63c
mamarilla2022 <mamarilla@netnix.net>
2022-10-11 11:59:14 -0300

    agregué js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//declaracion
+let nombre;
+let apellido;
+let edad;
+let anio;
+//asignacion
+nombre = "Jose";
+apellido = "Perez";
+edad = "33";
+anio = "1988";
+
+console.log(anio);
+
+//inicializar
+let mascota = "firulais"
+
+//const
+const PI=3.145123456
+const IVA=21
+const DNI=34228687

e8742fe46d4cb022cf5b4faa02ec497a8edad0f6
mamarilla2022 <mamarilla@netnix.net>
2022-10-12 00:45:29 -0300

    pruebas de clases

--- js/main.js
+++ js/main.js
@@ -1,20 +1,84 @@
-//declaracion
-let nombre;
-let apellido;
-let edad;
-let anio;
-//asignacion
-nombre = "Jose";
-apellido = "Perez";
-edad = "33";
-anio = "1988";
+// //declaracion
+// //let nombre;
+// let apellido;
+// let edad;
+// let anio;
+// //asignacion
+// //nombre = "Jose";
+// apellido = "Perez";
+// edad = "33";
+// anio = "1988";
+// //console.log(nombre)
+// console.log(apellido)
+// console.log(edad)
+// console.log(anio)
 
-console.log(anio);
 
-//inicializar
-let mascota = "firulais"
+// //inicializar
+// let mascota = "firulais"
 
-//const
-const PI=3.145123456
-const IVA=21
-const DNI=34228687
+// //const
+// const PI=3.145123456
+// const IVA=21
+// const DNI=34228687
+
+// let numero1= 34
+// let numero2= 76
+// let resultado= numero1 + numero2;
+// let resta= numero1 - numero2;
+// let multiplicar= numero1 * numero2;
+// console.log(multiplicar)
+// console.log(resta)
+// console.log(resultado)
+
+// //alert("hola")
+// //let entrada= prompt ("Ingrese su nombre")
+// //let mensaje="hola "
+// //let bienvenida=" bienvenida!! ingrese su clave"
+// //console.log(mensaje + entrada)
+// //prompt (mensaje + entrada + bienvenida)
+
+// //condicional
+// //if (true){
+// //console.log("pruebas del if")
+// //}
+
+// let numeroDePrueba= 10
+// let esIgual= numeroDePrueba == 10
+// console.log(esIgual)
+
+// //if (numeroDePrueba == 10){
+//     //console.log("esta transaccion puede realizarse correctamente")
+// //}
+
+// //let nombre = prompt ("ingresá tu nombre");
+// //if (nombre == "")
+//    // {
+//    // alert("no puso ningun nombre");
+// //}
+// //else
+//    // {
+//    // alert("hola " + nombre);
+
+    
+// //}
+// // let producto1 = 2.99;
+// // if(producto1 < 20) {
+// //     console.log("el precio es menor");
+// // }
+// // else if(producto1 >= 20){
+// //     console.log("el precio es mayor");
+// }
+// let ingreso= parseInt (prompt("ingrese un numero"))
+// for (i=0; i<=10; i= i+1){
+//     let resultado= ingreso * i;
+//     console.log(ingreso + "X" +i + "=" + resultado);
+// }
+
+let ingreso = prompt("ingrese producto");
+
+while (ingreso != "salir"){
+    
+    console.log(ingreso);
+    ingreso=prompt("ingrese producto")
+}
